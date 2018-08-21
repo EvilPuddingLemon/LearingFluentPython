a7a9af694800982a04d1d5bf8b7e1a3e29059c44
李业涛 <liyetao@corp.netease.com>
2018-08-21 11:59:30 +0800

    dddd

---
+++ S15/s15_1.py
@@ -0,0 +1,17 @@
+'''
+# 循环中使用else子句
+for item in my_list:
+    if item.flavor == 'banana':
+        break
+else:
+    raise ValueError('No banana flavor found!')
+'''
+'''
+# 清晰控制try中只包含可能抛出预期异常的子句
+try:         
+    dangerous_call()     
+except OSError:         
+    log('OSError...')     
+else:         
+    after_call()
+'''
